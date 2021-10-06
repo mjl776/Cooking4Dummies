0f56cbde4de5f6dfd16770ff44dac21235a0140b
Stefan Mrakovcic <sm7541@nyu.edu>
2021-10-06 10:39:37 -0400

    updateRetrevial

--- Run.java
+++ Run.java
@@ -1,11 +1,14 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.String;
 
-public class Run{
+public class Run {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Cooking4Dummies! Type 'c' to create a recipe and 'r' to retrieve a recipe.");
         String ans = sc.nextLine().toLowerCase();
+        ArrayList<Recipe> recipes = new ArrayList<Recipe>;
 
         //Invalid input
         ans = invalidInput(ans, "c", "r");
@@ -13,6 +16,10 @@ public class Run{
         //Create a recipe
         if (ans.equals("c")) {
             System.out.println("Let's create a recipe for you...");
+            // when a new recipe is created using a function in the Recipe class
+            // it then gets pushed onto the ArrayList of recipes
+            // don't forget to calculate the num object value by checking for the length of
+            // the ArrayList and adding 1. 
 		}
         //Retrieve a recipe
         else if (ans.equals("r")){
@@ -25,10 +32,28 @@ public class Run{
             //Search recipe by name
             if (recipeans.equals("s")){
                 System.out.println("Searching a recipe by name...");
+                Recipe recipe = byName(recipes);
+                if (recipe == null) {
+                    System.out.println("No recipe matches your search :(");
+                } else {
+                    // remember to make a toString method for the recipe in your Recipe class
+                    System.out.println("Here is your recipe: \n" + recipe.toString);
+                }
+                // add a loop if you want to make the user's life easy
+                //rather than starting over again redo the search
             }
             //Browse all recipes
             else if (recipeans.equals("b")){
                 System.out.println("Browse all recipes");
+                Recipe recipe = browseAll(recipes);
+                if (recipe == null) {
+                    System.out.println("That number is not in our catalogue. ");
+                } else {
+                    // remember to make a toString method for the recipe in your Recipe class
+                    System.out.println("Here is your recipe: \n" + recipe.toString);
+                }
+                // add a loop if you want to make the user's life easy
+                // wrong input so that they can try again
             }
             System.out.println("Type 'a' to read a recipe all at once or 's' to read through the recipe step-by-step.");
             String readans = sc.nextLine().toLowerCase();
@@ -57,4 +82,31 @@ public class Run{
         }
         return invalidString;
     }
+
+    public static Recipe byName(ArrayList<Recipe> recipes) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("search for a recipe by name :) : ");
+        String input = sc.nextLine().toLowerCase();
+        for (Recipe recipe : recipes) {
+            if (recipe.name.toLowerCase.contains(input)) {
+                return recipe;
+            } 
+        }
+        return null;
+    }
+
+    public static Recipe browseAll(ArrayList<Recipe> recipes) {
+        System.out.println("Here are all of our recipes. Please select by number. :)");
+        for (Recipe recipe : recipes) {
+            System.out.println(recipe.num + " ." + recipe.name);
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("select your recipe: ");
+        Int input = sc.nextLine().toLowerCase();
+        for (Recipe recipe : recipes) {
+            if (recipe.num == input) {
+                return recipe;
+            }
+        }
+        return null;
 }
